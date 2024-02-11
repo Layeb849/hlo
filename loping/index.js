3d0b5b74cfc9fb429926a4c2d6681225fe07b83f
Layeb849 <layebhossain849@gmail.com>
2024-02-11 21:12:41 +0600

    commited done

---
+++ loping/index.js
@@ -0,0 +1,9 @@
+
+
+// for(var i = 0; i < 10; i++)
+    function result() {
+        var a = document.getElementById("feeling").value;
+        for(var i = 0; i < 10; i++){
+            document.write(a + '<br>')
+        }
+}
